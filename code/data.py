7a910a68d5736c740f66e8d93ceade5b4d6b549f
Raphael Eißler <r.eissler@gmx.de>
2020-06-25 10:50:38 +0200

    fixed bug while creating the statistics dict

--- code/data.py
+++ code/data.py
@@ -5,7 +5,7 @@ This file containts dicts, lists or functions related to data.
 This should have the advance, if you work with multiple notebooks you can import the data from here and don't have to create them again and again
 '''
 
-from consts import DEVICES_MUSE_LSL, DEVICES_OPEN_BCI, DEVICES_NEUROSCAN
+from consts import DEVICES_MUSE_LSL, DEVICES_OPEN_BCI, DEVICES_NEUROSCAN, DEVICES_MUSE_LSL_OPEN_VIBE
 
 testSubjectDict = { 1 : {"Device" : DEVICES_OPEN_BCI,
                      "awakeCsvPath" : "D:/OneDrive - bwedu/Masterthesis/Experiments+Data/Fahren+AimLab/2020_03_05_Propand_1/openBci_record-[2020.03.05-12.27.35]_raw_awake_aimlab.csv",
@@ -24,3 +24,9 @@ onlineEegDataDict = { 1 : {"Device" : DEVICES_NEUROSCAN,
                       2 : {"Device" : DEVICES_NEUROSCAN,
                            "normalCsvPath" : "D:/Masterthesis/EEG_Data/eeg_data_online/2/Normal_state_256hz.csv",
                            "fatigueCsvPath" : "D:/Masterthesis/EEG_Data/eeg_data_online/2/Fatigue_state_256hz.csv"}}
+
+
+MuseExperimentDataDict = { 2 : {"Device" : DEVICES_MUSE_LSL_OPEN_VIBE,
+                                "unlabeledCsvPath" : "D:/Masterthesis/EEG_Data/muse_data/subject_2/reaction_game_complete.csv"},
+                           5 : {"Device" : DEVICES_MUSE_LSL_OPEN_VIBE,
+                                "unlabeledCsvPath" : "D:/Masterthesis/EEG_Data/muse_data/subject_5/reaction_game_complete.csv"}}
